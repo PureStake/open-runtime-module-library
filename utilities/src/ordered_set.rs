d0ddcb5ba788cada488b52c26494e63f43294e47
girazoki <gorka.irazoki@gmail.com>
2022-10-26 18:34:40 +0200

    Avoid error formatting

--- utilities/src/ordered_set.rs
+++ utilities/src/ordered_set.rs
@@ -25,7 +25,7 @@ impl<T: Ord + Encode + Decode + MaxEncodedLen + Clone + Eq + PartialEq, S: Get<u
 		v.sort();
 		v.dedup();
 
-		Self::from_sorted_set(v.try_into().expect("Did not add any values"))
+		Self::from_sorted_set(v.try_into().map_err(|_| ()).expect("Did not add any values"))
 	}
 
 	/// Create a set from a `Vec`.
